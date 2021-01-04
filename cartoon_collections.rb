446671177a51486bb9434e06ee2fc2825910f4ec
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 11:13:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ end
 def summon_captain_planet(small_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  small_calls.map do |element|
-    puts "#{element}!"
+  small_calls.collect do |small|
+   # puts "#{small}!"
+   small.capitalize + "!"
   end
 end
 
